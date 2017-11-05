8698a91c1d971a72c99ab2bf1cfa97dfdfa35812
Taiki Ono <taiks.4559@gmail.com>
2017-11-06 01:39:33 +0900

    Use .zero?

--- test/app/app.rb
+++ test/app/app.rb
@@ -2,11 +2,11 @@ require 'sinatra'
 
 get '/' do
   sleep ENV['SLEEP'].to_f
-  raise 'error' if rand(0..ENV['ERROR_RATE'].to_i) == 0
+  raise 'error' if rand(0..ENV['ERROR_RATE'].to_i).zero?
   "GET and #{ENV['RESPONSE'] || 'hello'}"
 end
 
 post '/' do
-  raise 'error' if rand(0..ENV['ERROR_RATE'].to_i) == 0
+  raise 'error' if rand(0..ENV['ERROR_RATE'].to_i).zero?
   "POST and #{ENV['RESPONSE'] || 'hello'}"
 end
