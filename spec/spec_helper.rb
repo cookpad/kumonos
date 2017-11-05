193bc43fc8c3c391f8ef307cc1a0a7a206ba74af
Taiki Ono <taiks.4559@gmail.com>
2017-11-06 01:50:54 +0900

    rubocop -a

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,9 +18,7 @@ RSpec.configure do |config|
   config.disable_monkey_patching!
   config.warnings = false
 
-  if config.files_to_run.one?
-    config.default_formatter = "doc"
-  end
+  config.default_formatter = 'doc' if config.files_to_run.one?
 
   config.profile_examples = 10
   config.order = :random
