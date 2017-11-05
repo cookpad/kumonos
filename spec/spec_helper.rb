84cad1ef96bc45a9f0ecdced8fd2b1a3a6c3d8a4
Taiki Ono <taiks.4559@gmail.com>
2017-11-06 01:43:58 +0900

    rspec --init

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,15 +4,27 @@ require 'kumonos'
 require 'rspec/json_matcher'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
-  config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = 'tmp/examples.txt'
+  config.disable_monkey_patching!
+  config.warnings = false
+
+  if config.files_to_run.one?
+    config.default_formatter = "doc"
+  end
+
+  config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
 
   config.include RSpec::JsonMatcher
 end
