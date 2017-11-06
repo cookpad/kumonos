58e438fa1637aa821be82c3aa16612f35c89446c
Taiki Ono <taiks.4559@gmail.com>
2017-11-07 00:21:43 +0900

    Validate service definition

--- lib/kumonos/schemas.rb
+++ lib/kumonos/schemas.rb
@@ -6,10 +6,16 @@ module Kumonos
   module Schemas
     ROOT = Pathname.new(File.expand_path('../schemas', __dir__))
     CONFIG_SCHEMA_PATH = ROOT.join('kumonos_config.json')
+    SERVIVE_DEFINITION_PATH = ROOT.join('service_definition.json')
 
     class << self
       def validate_kumonos_config(hash)
         schema = load_schema(CONFIG_SCHEMA_PATH)
+        JSON::Validator.fully_validate(schema, hash)
+      end
+
+      def validate_service_definition(hash)
+        schema = load_schema(SERVIVE_DEFINITION_PATH)
         JSON::Validator.fully_validate(schema, hash)
       end
 
