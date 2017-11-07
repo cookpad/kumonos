bbf431c82e15643477036e3c115bf0431e182d41
Taiki Ono <taiks.4559@gmail.com>
2017-11-07 10:18:00 +0900

    Allow tls option in envoy definition

--- lib/kumonos/envoy_definition.rb
+++ lib/kumonos/envoy_definition.rb
@@ -2,16 +2,25 @@ module Kumonos
   EnvoyDefinition = Struct.new(:version, :ds, :statsd, :listener, :admin) do
     class << self
       def from_hash(h)
+        ds = symbolize_keys(h.fetch('ds'))
+        convert_tls_option!(ds.fetch(:cluster))
+
         new(
           h.fetch('version'),
-          symbolize_keys(h.fetch('ds')),
-          symbolize_keys(h.fetch('statsd', {})),
+          ds,
+          convert_tls_option!(symbolize_keys(h.fetch('statsd', {}))),
           symbolize_keys(h.fetch('listener')),
           symbolize_keys(h.fetch('admin'))
         )
       end
 
       private
+
+      def convert_tls_option!(cluster)
+        tls = cluster.delete(:tls)
+        cluster[:ssl_context] = {} if tls
+        cluster
+      end
 
       def symbolize_keys(hash)
         new = hash.map do |k, v|
